cab20596cd106fa08eac8c76e461b8adbddec3a2
jwj0320 <jwj0320@ajou.ac.kr>
2022-05-09 12:27:49 +0900

    add org_create panel

--- demo/src/main/java/gui/tabbedContent/OrgPanel.java
+++ demo/src/main/java/gui/tabbedContent/OrgPanel.java
@@ -3,6 +3,10 @@ package gui.tabbedContent;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.awt.event.ActionEvent;
+import java.awt.Component;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -14,6 +18,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 
 import io.github.qualtagh.swing.table.model.IModelFieldGroup;
 import io.github.qualtagh.swing.table.model.ModelData;
@@ -51,12 +57,11 @@ public class OrgPanel extends GridBagPanel {
         JButton createButton = new JButton("Create");
         addGBLComponent(createButton, 5, 1, 0.005, 0.1);
 
-
         IModelFieldGroup groups[] = new IModelFieldGroup[] {
                 new ModelField("Business Process", "Business Process"),
                 new ModelFieldGroup("Human", "Human")
                         .withChild(new ModelField("Role", "Role"))
-                        .withChild(new ModelField("Person", "Person").withRowspan(2)), // Custom rowspan set.
+                        .withChild(new ModelField("Person", "Person")), // Custom rowspan set.
                 new ModelFieldGroup("Information Technology", "Information Technology")
                         .withChild(new ModelField("Software", "Software"))
                         .withChild(new ModelField("Data", "Data"))
@@ -87,6 +92,99 @@ public class OrgPanel extends GridBagPanel {
         JButton nextButton = new JButton("Next");
         addGBLComponent(nextButton, 6, 3, 0.5, 0.1, "NONE", GridBagConstraints.LINE_END);
 
-    }
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GridBagPanel createPanel = new GridBagPanel(tabbedPane);
 
+                createPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+                JLabel infoLabel = new JLabel();
+                infoLabel.setText("");
+
+                createPanel.addGBLComponent(infoLabel, 0, 0, 5, 1, 0.0, 0.1, "NONE", GridBagConstraints.LINE_START);
+
+                JLabel orgLabel = new JLabel();
+                orgLabel.setText("Organizaion");
+                createPanel.addGBLComponent(orgLabel, 1, 1, 0.01, 0.1);
+
+                JComboBox<Object> orgComboBox = new JComboBox<>(); // Object는 추후에 수정
+                orgComboBox.addItem("Sample Company");
+                orgComboBox.setPreferredSize(new Dimension(250, 22));
+                createPanel.addGBLComponent(orgComboBox, 2, 1, 0.0, 0.1);
+
+
+                IModelFieldGroup groups[] = new IModelFieldGroup[] {
+                        new ModelField("Business Process", "Business Process"),
+                        new ModelFieldGroup("Human", "Human")
+                                .withChild(new ModelField("Role", "Role"))
+                                .withChild(new ModelField("Person", "Person")), // Custom rowspan set.
+                        new ModelFieldGroup("Information Technology", "Information Technology")
+                                .withChild(new ModelField("Software", "Software"))
+                                .withChild(new ModelField("Data", "Data"))
+                                .withChild(new ModelField("Platform", "Platform"))
+                                .withChild(new ModelField("Hardware", "Hardware")),
+                        new ModelField("Physical & Environment", "Physical & Environment")
+                };
+
+                // Get leafs of columns tree.
+                ModelField fields[] = ModelFieldGroup.getBottomFields(groups);
+
+                String[][] innerValue;
+                String[] temp = null;
+                // Sample data.
+                ModelRow rows[] = new ModelRow[0];
+                for (int i = 0; i < rows.length; i++) {
+                    rows[i] = new ModelRow(fields.length);
+                    for (int j = 0; j < fields.length; j++) {
+                        // rows[i].setValue(j, fields[j].getCaption() + i);
+                        innerValue = new String[3][1];
+                        temp = new String[1];
+                        temp[0] = fields[j].getCaption();
+                        innerValue[0] = temp;
+                        innerValue[1] = temp;
+                        innerValue[2] = temp;
+                        rows[i].setValue(j, innerValue);
+                    }
+                }
+
+                // Table.
+                ModelData data = new ModelData(groups);
+                data.setRows(rows);
+                JBroTable table = new JBroTable(data);
+                table.getTableHeader().setEnabled(false);
+                table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+                    @Override
+                    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+                            boolean hasFocus, int row, int column) {
+                        String[] headers = { "" };
+                        String[][] rows = (String[][]) value;
+                        String[] t = (String[])value;
+                        // DefaultTableModel model = new DefaultTableModel(columns,0);
+                        JTable insideTable = new JTable(rows, headers);
+
+                        table.setRowHeight(row, 20 + (15 * insideTable.getRowCount()));
+                        JScrollPane columnFix = new JScrollPane(insideTable);
+                        columnFix.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+
+                        return columnFix;
+                    }
+                });
+
+                JScrollPane tableScPane = table.getScrollPane();
+                // tableScPane.setPreferredSize(new Dimension(10,46));
+
+                createPanel.addGBLComponent(tableScPane, 0, 2, 9, 1, 0.0, 0.4, "BOTH");
+
+                JButton cancelButton = new JButton("Cancel");
+                createPanel.addGBLComponent(cancelButton, 6, 3, 0.8, 0.1,"NONE", GridBagConstraints.LINE_END);
+                JButton applyButton = new JButton("Apply");
+                createPanel.addGBLComponent(applyButton, 7, 3, 0.05, 0.1);
+                JButton confirmButton = new JButton("Confirm");
+                createPanel.addGBLComponent(confirmButton, 8, 3, 0.05, 0.1);
+                tabbedPane.setComponentAt(tabbedPane.getSelectedIndex(), createPanel);
+
+            }
+        });
+    }
 }
